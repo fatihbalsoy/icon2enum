b2097dc3d0abacbd4416718f190fe6142542924b
Muhammet Balsoy <28816254+fatihbalsoy@users.noreply.github.com>
2020-06-29 16:08:26 -0500

    Kotlin support

--- templates/template.dart
+++ templates/template.dart
@@ -1,5 +1,6 @@
 
 class template {
+    // MARK: - HEAD_LINE
     static var THIS_LINE = "";
     static var LAST_LINE = "";
 }
---
+++ templates/template.kt
@@ -0,0 +1,7 @@
+
+enum class template(val hex : String) {
+    
+    // MARK: - HEAD_LINE
+    THIS_LINE(""),
+    LAST_LINE("")
+}
