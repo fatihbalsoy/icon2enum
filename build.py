594c0a01dfbcf4280cee2865df02c5ee2132fb71
Muhammet Balsoy <28816254+fatihbalsoy@users.noreply.github.com>
2020-06-29 02:04:56 -0500

    New syntax class and testing progress bar

--- build.py
+++ build.py
@@ -12,48 +12,57 @@ soup = BeautifulSoup(html_page, "html.parser")
 
 
 def getIcons():
-    script = soup.findAll('script')[0]
-    icons0 = str(script).split("var icons = [")
-    icons1 = icons0[1].split("]")[0]
-    icons = icons1.split("},{")
-
-    array = []
-    for i in icons:
-        cleaned = i.replace("{", "")
-        cleaned = cleaned.replace("}", "")
-        keys = cleaned.split("\"")
-
-        name = keys[1]
-        hex = keys[3]
-        version = keys[5]
-
-        uppercamelcased = ''.join(word.title() for word in name.split('-'))
-        camelcased = uppercamelcased[0].lower() + uppercamelcased[1:]
-        snakecased = '_'.join(word.title() for word in name.split('-'))
-        snakecased = snakecased.upper()
-
-        unicode = "\"\\u{" + hex + "}\""
-        array.append([name, hex, version, uppercamelcased,
-                      camelcased, snakecased, unicode])
-
-    return array
-
-
-def generateFile(lang):
-    ext = getExtension(lang)
-    with open("MDIcons." + ext, "w") as file:
-        cl = generateClass(lang)
-        icons = getIcons()
-        enums = ""
-        for icon in icons:
-            enum = generateEnum(getExtension(lang), icon)
-            enums += "\t" + enum + "\n"
-
-        final = cl.replace("%s", enums)
-        print(final)
-        file.write(final)
-
-
+	script = soup.findAll('script')[0]
+	icons0 = str(script).split("var icons = [")
+	icons1 = icons0[1].split("]")[0]
+	icons = icons1.split("},{")
+	
+	array = []
+	for i in icons:
+		cleaned = i.replace("{", "")
+		cleaned = cleaned.replace("}", "")
+		keys = cleaned.split("\"")
+		
+		name = keys[1]
+		hex = keys[3]
+		version = keys[5]
+		
+		uppercamelcased = ''.join(word.title() for word in name.split('-'))
+		camelcased = uppercamelcased[0].lower() + uppercamelcased[1:]
+		snakecased = '_'.join(word.title() for word in name.split('-'))
+		snakecased = snakecased.upper()
+		
+		unicode = "\"\\u{" + hex + "}\""
+		array.append([name, hex, version, uppercamelcased,
+		camelcased, snakecased, unicode])
+		
+	return array
+	
+	
+def generateFile(syntax):
+	ext = syntax.getExtension()
+	with open("MDIcons." + ext, "w") as file:
+		cl = syntax.generateClass()
+		icons = getIcons()
+		enums = ""
+		x = 0
+		for icon in icons:
+			enum = syntax.generateEnum(icon)
+			enums += "\t" + enum + "\n"
+			name = icon[IconData.name]
+			#print(f'{icon[IconData.name]}\r', end="")
+			#print('{}\r'.format(name), end="")
+			#print("Progress {:2.1%}".format(x / len(icons)), end="\r")
+			x += 1
+		print()
+		
+		final = cl.replace("%s", enums)
+		#print(final)
+		file.write(final)
+		
+		
 print("Specify the language to be generated:")
 print("(swift, java, kotlin, c++, go, and etc.)")
-generateFile(input())
+syntax = Syntax(input())
+generateFile(syntax)
+
--- helpers/syntax.py
+++ helpers/syntax.py
@@ -3,43 +3,61 @@ from helpers.icon import *
 
 current_dir = os.path.dirname(__file__)
 
-def getExtension(lang):
-    if lang == "c++":
-        return "cpp"
-    if lang == "kotlin":
-        return "kt"
-    if lang == "javascript":
-        return "js"
-    if lang == "ruby":
-        return "rb"
-    if lang == "typescript":
-        return "ts"
-    if lang == "python":
-        return "py"
-    return lang
+class Syntax:
 
-def generateClass(lang):
-    ext = getExtension(lang)
-    with open(current_dir + "/../lang/template." + ext, "r") as file:
-        return file.read()
+	lang = ""
+	
+	def __init__(self, lang):
+		self.lang = lang
+		
+	def getExtension(self):
+		lang = self.lang
+		if lang == "c++":
+			return "cpp"
+		if lang == "kotlin":
+			return "kt"
+		if lang == "javascript":
+			return "js"
+		if lang == "ruby":
+			return "rb"
+		if lang == "typescript":
+			return "ts"
+		if lang == "python":
+			return "py"
+		return lang
+		
+	def generateClass(self):
+		lang = self.lang
+		ext = self.getExtension()
+		with open(current_dir + "/../lang/template." + ext, "r") as file:
+			return file.read()
+			
+	def getUnicode(self, hex):
+		lang = self.lang
+		if lang is "swift":
+			return "\\u{" + hex + "}"
+		if lang is "java":
+			return "\\u" + hex
+			
+	def generateEnum(self, icon):
+		lang = self.getExtension()
+		camelcased = icon[IconData.camelcased]
+		snakecased = icon[IconData.snakecased]
+		uppercamel = icon[IconData.uppercamelcased]
+		unicode = icon[IconData.unicode]
+		
+		if lang == "swift":
+			return "case " + camelcased + " = " + unicode
+		if lang == "cpp":
+			return camelcased + " = " + unicode
+		if lang in ["java", "kt"]:
+			return snakecased + "(" + unicode + ")"
+		if lang in ["js", "rb"]:
+			return snakecased + ": " + unicode
+		if lang in ["ts", "go"]:
+			return uppercamel + " = " + unicode
+		if lang == "py":
+			return snakecased + " = " + unicode
+		if lang == "objc":
+			return "case " + camelcased + ": " + "\n\treturn @" + unicode + ";"
 
-def generateEnum(lang, icon):
-    camelcased = icon[IconData.camelcased]
-    snakecased = icon[IconData.snakecased]
-    uppercamel = icon[IconData.uppercamelcased]
-    unicode = icon[IconData.unicode]
-
-    if lang == "swift":
-        return "case " + camelcased + " = " + unicode
-    if lang == getExtension("c++"):
-        return camelcased + " = " + unicode
-    if lang in ["java", getExtension("kotlin")]:
-        return snakecased + "(" + unicode + ")"
-    if lang in [getExtension("javascript"), getExtension("ruby")]:
-        return snakecased + ": " + unicode
-    if lang in [getExtension("typescript"), "go"]:
-        return uppercamel + " = " + unicode
-    if lang == getExtension("python"):
-        return snakecased + " = " + unicode
-    if lang in ["objc"]:
-        return "case " + camelcased + ": " + "\n\treturn @" + unicode + ";"
